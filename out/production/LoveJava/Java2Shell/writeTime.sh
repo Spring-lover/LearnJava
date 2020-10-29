7ee397bfe35de13280b3534fbf90f076cd961ca2
hujiale <hjlbupt@163.com>
2020-10-29 17:54:25 +0800

    update

--- out/production/LoveJava/Java2Shell/writeTime.sh
+++
@@ -1,5 +0,0 @@
-javac HelloShell.java
-
-cd ..
-
-nohup java Java2Shell/HelloShell > ./writeTest.log 2>&1 &
--- out/production/LoveJava/Javascript/code.js
+++
@@ -1,6 +0,0 @@
-let i = 0
-const checkInterval = setInterval(()=>{
-	i += 1
-	console.log(i)
-	if(i === 3) clearInterval(checkInterval)
-}, 1000)
